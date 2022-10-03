f8460435bb4906c7a50c8ee7ae362ceb22aeebab
Tairone Adamo de Araujo <83379500+taironearaujo@users.noreply.github.com>
2022-10-02 21:51:37 -0300

    Add files via upload

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,12 +3,18 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        // DONE: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string Imei { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            // DONE: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
